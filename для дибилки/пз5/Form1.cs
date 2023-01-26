767b4de81b8c17dd757b28828824cfd23472ad0a
S1delnikov <92618363+S1delnikov@users.noreply.github.com>
2023-01-26 19:06:53 +0300

    Add files via upload

---
+++ для дибилки/пз5/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace пз5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = "-1,5";
+            textBox2.Text = "-2,5";
+            textBox3.Text = "-0,5";
+            textBox4.Text = "-0,8";
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double x0 = Convert.ToDouble(textBox1.Text);
+            double xk = Convert.ToDouble(textBox2.Text);
+            double dx = Convert.ToDouble(textBox3.Text);
+            double b = Convert.ToDouble(textBox4.Text);
+            textBox5.Text = "Решение: " + Environment.NewLine;
+
+            while (x0 >= xk)
+            {
+                double y = Math.Pow(x0, 2) + Math.Tan(5 * x0 + b / x0);
+                textBox5.Text += "x = " + Convert.ToString(x0) + "; y = " + Convert.ToString(y) +
+                    Environment.NewLine;
+                x0 = x0 + (dx);
+
+            }
+        }
+    }
+}
---
+++ для дибилки/пз5/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("пз5")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("пз5")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("f19cc1fc-b168-4a1b-a2a4-15fc30d1228a")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
