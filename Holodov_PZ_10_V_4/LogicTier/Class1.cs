c39076b0d135c26bacf9a29766e598b459bd399e
S1delnikov <92618363+S1delnikov@users.noreply.github.com>
2023-03-16 21:00:31 +0300

    Add files via upload

---
+++ Holodov_PZ_10_V_4/LogicTier/Class1.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DataTier;
+
+namespace LogicTier
+{
+    public class БилетнаяПозиция
+    {
+        private Билет _билет;
+
+        public БилетнаяПозиция(Билет p)
+        {
+            _билет = p;
+        }
+        public string Транспорт
+        {
+            get { return _билет.Транспорт; }
+            set { _билет.Транспорт = value; }
+        }
+        public string ПунктНазначения
+        {
+            get { return _билет.Пункт_назначения; }
+            set { _билет.Пункт_назначения = value; }
+        }
+        public float ЦенаБилета
+        {
+            get { return _билет.Цена; }
+            set { _билет.Цена = value; }
+        }
+        public string ПунктОтправки
+        {
+            get { return _билет.Пункт_отправки; }
+            set { _билет.Пункт_отправки = value; }
+        }
+
+        public string ПредставлениеТовара
+        {
+            get
+            {
+                return _билет.Пункт_отправки + " - " + _билет.Пункт_назначения;
+            }
+        }
+
+        public int КоличествоАвтоРейсов
+        {
+            get { return _билет.КолВоБилетовНаАвтобус; }
+            set { _билет.КолВоБилетовНаАвтобус = value; }
+        }
+
+        public double СуммаБилетовНаСамолёт
+        {
+            get { return _билет.СуммаБилетовНаСамолёт; }
+            set { _билет.СуммаБилетовНаСамолёт = value; }
+        }
+
+        public double СамыйДорогойБилет
+        {
+            get { return _билет.СамыйДорогойБилет; }
+            set { _билет.СамыйДорогойБилет = value; }
+        }
+    }
+
+    public class Касса
+    {
+        private List<БилетнаяПозиция> _билеты = new List<БилетнаяПозиция>();
+        public Касса()
+        {
+            List<Билет> tmp = ВсеБилеты.ПолучитьВсеБилеты();
+            foreach (var t in tmp)
+            {
+                _билеты.Add(new БилетнаяПозиция(t));
+            }
+        }
+        public List<БилетнаяПозиция> СписокБилетов
+        {
+            get { return _билеты; }
+        }
+        public string НаименованиеМагазина
+        {
+            get { return "Вокзал"; }
+        }
+    }
+}
---
+++ Holodov_PZ_10_V_4/PresentationTier/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using LogicTier;
+
+namespace PresentationTier
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            Касса logicTier = new Касса();
+            this.DataContext = logicTier;
+            InitializeComponent();
+        }
+    }
+}
