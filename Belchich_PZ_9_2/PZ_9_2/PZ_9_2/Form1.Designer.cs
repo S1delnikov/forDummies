548a55d310df8ddabefd57b76dd73f55d99b9995
S1delnikov <92618363+S1delnikov@users.noreply.github.com>
2023-04-06 20:37:21 +0300

    Add files via upload

---
+++ Belchich_PZ_9_2/PZ_9_2/PZ_9_2/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace PZ_9_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(776, 350);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(49, 378);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(49, 407);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(174, 378);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(533, 56);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.Value = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ Belchich_PZ_9_2/PZ_9_2/PZ_9_2/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace PZ_9_2
+{
+    public partial class Form1 : Form
+    {
+        GraphicsPath path;
+        public Form1()
+        {
+            InitializeComponent();
+            path = new GraphicsPath();
+        }
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics gr = e.Graphics;
+            gr.Clear(Color.White);
+            //Создаём штриховую кисть жёлтого цвета
+            HatchBrush brush = new HatchBrush(myBrush, myColor);
+            //gr.FillEllipse(brush, x0 - radius, y0 - radius, 2 * radius, 2 * radius);
+            Point[] pointer = new Point[]
+            {
+                new Point (x0, y0),
+                new Point (x0 + 50, y0),
+                new Point (x0 + 50, y0 - 10),
+                new Point (x0 + 75, y0 + 8),
+                new Point (x0 + 50, y0 + 25),
+                new Point (x0 + 50, y0 + 15),
+                new Point (x0, y0 + 15),
+                new Point (x0, y0 + 25),
+                new Point (x0 - 25, y0 + 8),
+                new Point (x0, y0 - 10),
+                new Point (x0, y0)
+            };
+            gr.DrawLines(new Pen(Color.Black, 2), pointer);
+            gr.FillPolygon(brush, pointer);
+
+            // Очищаем фигуру
+            path.Reset();
+            // Начинаем формирование фигуры
+            path.StartFigure();
+            // Добавляем круг в фигуру
+            path.AddLines(pointer);
+            // Завершаем формирование фигуры
+            path.CloseFigure();
+        }
+        int x0, y0, xDir, yDir;
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            timer1.Interval = trackBar1.Value;
+        }
+
+        HatchStyle myBrush;
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Point pt = new Point(e.X, e.Y);
+                if (path.IsVisible(pt))
+                {
+                    ColorDialog clrDlg = new ColorDialog();
+                    if (clrDlg.ShowDialog() == DialogResult.OK)
+                        myColor = clrDlg.Color;
+                }
+            }
+            if (e.Button == MouseButtons.Right)
+            {
+                Point pt = new Point(e.X, e.Y);
+                if (path.IsVisible(pt))
+                {
+                    Form2 styleDialog = new Form2();
+                    if (styleDialog.ShowDialog() == DialogResult.OK)
+                        myBrush = styleDialog.Style;
+                }
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        int loop = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (loop == 0)
+            {
+                if (x0 != 25)
+                    x0--;
+                else loop++;
+            }
+            if (loop == 1)
+            {
+                if (x0 != centerX)
+                    x0++;
+                else loop++;
+            }
+            if (loop == 2)
+            {
+                if (y0 != 10)
+                    y0--;
+                else loop++;
+            }
+            if (loop == 3)
+            {
+                if (y0 != centerY)
+                    y0++;
+                else loop++;
+            }
+            if (loop == 4)
+            {
+                if (x0 != pictureBox1.Width - 75)
+                    x0++;
+                else loop++;
+            }
+            if (loop == 5)
+            {
+                if (x0 != centerX)
+                    x0--;
+                else loop++;
+            }
+            if (loop == 6)
+            {
+                if (y0 != pictureBox1.Height - 25)
+                    y0++;
+                else loop++;
+            }
+            if (loop == 7)
+            {
+                if (y0 != centerY)
+                    y0--;
+                else loop = 0;
+            }
+
+            pictureBox1.Invalidate();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        Color myColor;
+        int centerX, centerY;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            centerX = pictureBox1.Width / 2 - 50;
+            centerY =pictureBox1.Height / 2;
+            x0 = pictureBox1.Width / 2 - 50;
+            y0 = pictureBox1.Height / 2;
+            myColor = Color.Yellow;
+        }
+    }
+}
